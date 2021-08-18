c4dc3c0f78e1ff0a17607e07bbb0e6d0dd0828fa
cola <jn123977@163.com>
2021-08-18 15:26:50 +0800

    Eureka服务集群、端口8002

---
+++ springcloud-provider-dept-8002/src/main/java/com/cola/springcloud/service/DeptService.java
@@ -0,0 +1,13 @@
+package com.cola.springcloud.service;
+
+import com.cola.springcloud.pojo.Dept;
+
+import java.util.List;
+
+public interface DeptService {
+    public boolean addDept(Dept dept);
+
+    public Dept queryById(Long id);
+
+    public List<Dept> queryAll();
+}
