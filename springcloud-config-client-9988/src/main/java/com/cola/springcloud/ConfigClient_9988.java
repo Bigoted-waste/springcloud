032fc5e3e66b95fb7739ea2cbb8d281027088776
cola <jn123977@163.com>
2021-08-19 23:22:04 +0800

    SpringCloud-Client

---
+++ springcloud-config-client-9988/src/main/java/com/cola/springcloud/ConfigClient_9988.java
@@ -0,0 +1,12 @@
+package com.cola.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClient_9988 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_9988.class,args);
+    }
+}
